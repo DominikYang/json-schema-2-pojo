e84994ef62408457a46a0a42611c1d83c2189a06
Wei Yuyang <wyy453822487@outlook.com>
2020-03-10 13:35:42 +0800

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 echo "$1 $2 $3 $4"
-java -jar json-schema-to-pojo-1.0-SNAPSHOT.jar jsonPath=$1 outPath=$2 packageNameVO=$3 packageNameDTO=$4
+#java -jar jsonschema2pojo.jar jsonPath=$1 outPath=$2 packageNameVO=$3 packageNameDTO=$4
